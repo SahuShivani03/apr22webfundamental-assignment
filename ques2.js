0905c880a4bc9c091efa524ab2b9254b7f69a904
shivani <sivani391999@gmail.com>
2022-06-19 14:06:28 +0530

    first commit

---
+++ ques2.js
@@ -0,0 +1,55 @@
+//2. Multiple records of people are given, you have to print the first name and last name . 
+let friends = 
+    [
+    {
+        fname: 'Rachal',
+        lname: "Green",
+        id: 1,
+        marks: 98,
+    },
+    {
+        fname: 'Ross',
+        lname: "Geller",
+        id: 2,
+        marks: 23,
+    },
+    {
+        fname: 'Monica ',
+        lname: "Geller",
+        id: 3,
+        marks: 75,
+    },
+    {
+        fname: 'Joey',
+        lname: " Tribbiani",
+        id: 4,
+        marks: 46,
+    },
+    {
+        fname: 'Chandler  ',
+        lname: "Bing",
+        id: 5,
+        marks: 78
+    },
+    {
+        fname: 'Phoebe',
+        lname: "Buffay",
+        id: 6,
+        marks: 45,
+    
+    }
+]
+
+console.log(friends[0].fname);
+console.log(friends[0].lname);
+console.log(friends[1].fname);
+console.log(friends[1].lname);
+console.log(friends[2].fname);
+console.log(friends[2].lname);
+console.log(friends[3].fname);
+console.log(friends[3].lname);
+console.log(friends[4].fname);
+console.log(friends[4].lname);
+console.log(friends[5].fname);
+console.log(friends[5].lname);
+
