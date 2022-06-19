883cb4d856122d126cb8bdd6b3ce3b01fe043900
Shivani sahu <102249991+SahuShivani03@users.noreply.github.com>
2022-06-19 14:04:07 +0530

    Add files via upload

---
+++ ques3.js
@@ -0,0 +1,3 @@
+//3.You are given some names of different cities and a ordered list html element; add a list item foreach to the ordered list.  (below html code and output is given)
+  city = ['Delhi', 'Mumbai', 'Jaipur','Kolkata','Indore' ];
+  console.log(city);
